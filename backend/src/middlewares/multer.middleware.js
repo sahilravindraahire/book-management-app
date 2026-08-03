fa8dd392e7a90fcf7ce52442caf31487174abcdd
sahil ahire <sahilahire2003@gmail.com>
2026-08-03 17:48:19 +0530

    changes in multer

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,26 +1,60 @@
-import multer from "multer"
-import path from "path"
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const tempDir = path.join(process.cwd(), "public", "temp");
+
+fs.mkdirSync(tempDir, { recursive: true });
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/temp")
-    },
-    filename:  function (req, file, cb) {
-        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e6)}`
-        cb(null, `${uniqueSuffix}${path.extname(file.originalname)}`)
-    }
-})
+  destination(req, file, cb) {
+    cb(null, tempDir);
+  },
+
+  filename(req, file, cb) {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e6)}`;
+    cb(null, `${uniqueSuffix}${path.extname(file.originalname)}`);
+  },
+});
 
 const fileFilter = (req, file, cb) => {
-    if(file.mimetype.startsWith("image/")){
-        cb(null, true)
-    } else {
-        cb(new Error("Only image files are allowed for a cover"), false)
-    }
-}
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed for a cover"), false);
+  }
+};
 
 export const upload = multer({
-    storage,
-    fileFilter,
-    limits: {fileSize: 10 * 1024 * 1024}  
-})
+  storage,
+  fileFilter,
+  limits: { fileSize: 10 * 1024 * 1024 },
+});
+
+
+// import multer from "multer"
+// import path from "path"
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, "./public/temp")
+//     },
+//     filename:  function (req, file, cb) {
+//         const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e6)}`
+//         cb(null, `${uniqueSuffix}${path.extname(file.originalname)}`)
+//     }
+// })
+
+// const fileFilter = (req, file, cb) => {
+//     if(file.mimetype.startsWith("image/")){
+//         cb(null, true)
+//     } else {
+//         cb(new Error("Only image files are allowed for a cover"), false)
+//     }
+// }
+
+// export const upload = multer({
+//     storage,
+//     fileFilter,
+//     limits: {fileSize: 10 * 1024 * 1024}  
+// })
