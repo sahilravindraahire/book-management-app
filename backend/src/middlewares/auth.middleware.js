fa78be7fa1e249beaddeb1209d8af7eaa5c2f5a9
sahil ahire <sahilahire2003@gmail.com>
2026-08-04 10:46:27 +0530

    middleare.js delete

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,26 +5,30 @@ import {asyncHandler} from "../utils/asynchandler.js"
 import {User} from "../models/user.model.js"
 
 export const verifyJWT = asyncHandler(async(req, res, next) => {
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-
-    if(!token){
-        throw new apiError(401, "Unauthorized request")
-    }
-
-    let decoded
-
     try {
-        decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    
+        if(!token){
+            throw new apiError(401, "Unauthorized request")
+        }
+    
+        let decoded
+    
+        try {
+            decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        } catch (error) {
+            throw new apiError(401, "Invalid or expired access token")
+        }
+    
+        const user = await User.findById(decoded._id).select("-password -refreshToken")
+    
+        if(!user){
+            throw new apiError(401, "Invalid access token: user not found")
+        }
+    
+        req.user = user
+        next()
     } catch (error) {
-        throw new apiError(401, "Invalid or expired access token")
+        throw new apiError(401, error?.message || "invalid access token")
     }
-
-    const user = await User.findById(decoded._id).select("-password -refreshToken")
-
-    if(!user){
-        throw new apiError(401, "Invalid access token: user not found")
-    }
-
-    req.user = user
-    next()
 })
