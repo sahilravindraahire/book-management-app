1b4c4977d7ff8ded99131183181508d15fa3bbb3
sahil ahire <sahilahire2003@gmail.com>
2026-08-04 11:17:40 +0530

    mext.config edited

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "https://book-management-backend-9jmz.onrender.com/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
